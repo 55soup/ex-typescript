dcf8cd3e56c7be3389d662312727731615e5950f
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-18 00:00:09 +0900

    타입지정하기

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-var 이름 = "kim";
+var 이름 = 123;
+function 함수(x) {
+    return x * 2;
+}
+함수(123);
+var john = [123, true];
---
+++ index.ts
@@ -0,0 +1,13 @@
+// let 이름: string = "kim";
+// let 이름: string[] = ["kim", "park"];
+// let 이름: { name?: string } = {};
+type Name = string | number;
+let 이름: Name = 123;
+
+function 함수(x: number): number {
+  return x * 2;
+}
+함수(123);
+
+type Member = [number, boolean];
+let john: Member = [123, true];
