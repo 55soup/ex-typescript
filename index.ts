0308e96dcf57da4c76a813ca62f3506769e682b8
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-19 23:37:01 +0900

    class문법

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 // let 이름: string = "kim";
 // let 이름: string[] = ["kim", "park"];
 // let 이름: { name?: string } = {};
-type Name = string | number;
-let 이름: Name = 123;
+// type Name = string | number;
+// let 이름: Name = 123;
 
-function 함수(x: number): number {
-  return x * 2;
+// function 함수(x: number): number {
+//   return x * 2;
+// }
+// 함수(123);
+
+// // type Member = [number, boolean];
+// // let john: Member = [123, true];
+
+// type Member = {
+//   [key: string]: string;
+// };
+// let john: Member = { name: "kim" };
+
+class User {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
 }
-함수(123);
-
-type Member = [number, boolean];
-let john: Member = [123, true];
