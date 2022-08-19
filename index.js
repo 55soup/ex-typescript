b5e866e0b211eeb95963682e62a22db12b232e2f
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-19 23:46:00 +0900

    변수 자료형

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-// let 이름: string = "kim";
-// let 이름: string[] = ["kim", "park"];
-// let 이름: { name?: string } = {};
-// type Name = string | number;
-// let 이름: Name = 123;
-// function 함수(x: number): number {
-//   return x * 2;
-// }
-// 함수(123);
-// // type Member = [number, boolean];
-// // let john: Member = [123, true];
-// type Member = {
-//   [key: string]: string;
-// };
-// let john: Member = { name: "kim" };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+var 이름 = "kim";
+var 나이 = 50;
+// let 결혼했니: boolean = true;
+var 결혼했니 = null;
--- index.ts
+++ index.ts
@@ -1,25 +1,4 @@
-// let 이름: string = "kim";
-// let 이름: string[] = ["kim", "park"];
-// let 이름: { name?: string } = {};
-// type Name = string | number;
-// let 이름: Name = 123;
-
-// function 함수(x: number): number {
-//   return x * 2;
-// }
-// 함수(123);
-
-// // type Member = [number, boolean];
-// // let john: Member = [123, true];
-
-// type Member = {
-//   [key: string]: string;
-// };
-// let john: Member = { name: "kim" };
-
-class User {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+let 이름: string = "kim";
+let 나이: number = 50;
+// let 결혼했니: boolean = true;
+let 결혼했니: undefined = null;
