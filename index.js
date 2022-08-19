0c5dd662ddab81299146b7754f32c3ac39443b6a
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-20 00:02:51 +0900

    퀴즈

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var 회원들 = {
-    member1: "kim",
-    member2: "park",
+// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
+var myName = "박선주";
+var age = 18;
+var hometown = "서울"; //타입지정 생략 가능
+// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
+var favorite = {
+    song: "로드파이터",
+    singer: "젝스키스",
 };
