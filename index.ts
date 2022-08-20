c4185f26501729c0396d55f5c91ed4a90ac0b709
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-20 23:38:46 +0900

    ts 함수

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
-// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
-let myName: string = "박선주";
-let age: number = 18;
-let hometown = "서울"; //타입지정 생략 가능
+function 함수(x) {
+  return x * 2;
+}
 
-// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
-let favorite: { song: string; singer: string } = {
-  song: "로드파이터",
-  singer: "젝스키스",
-};
+함수(30);
